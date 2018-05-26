9035b03e9d918919d07109ddefc37cdf39afa638
ksc <ksc@ksc-15Z970-TA50K>
2018-05-27 01:07:13 +0900

    request read

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +28,25 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+        	  BufferedReader reader = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+        	  DataOutputStream dos = new DataOutputStream(out);
+        	  
+        	  String line = "";
+        	  int index = 0;
+        	  String url = "";
+        	  while(!"".equals(line=reader.readLine())) {
+        		  if(line == null) {
+        			  return;
+        		  }
+        		  if(index == 0) {
+        			  String[] tokens = line.split(" ");
+        			  url = tokens[1];
+        		  }
+        		  log.debug("line : "+line);  
+        		  index++;
+        	  }
+        	  
+        	  byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
